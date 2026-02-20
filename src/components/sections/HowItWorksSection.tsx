2bb7395453b5d6de5ba0ce8165078cb37502eb1a
Gustavo Clement <clementgustavo1301@gmail.com>
2026-02-20 19:27:33 +0000

    desenvolva mais esse efeito ao passar, esta mal feito

--- src/components/sections/HowItWorksSection.tsx
+++ src/components/sections/HowItWorksSection.tsx
@@ -128,17 +128,18 @@ export default function HowItWorksSection() {
             </div>
           </div>
 
-          {/* Beams from Inputs to Center */}
+          {/* Beams */}
           {isHovered && (
             <>
-              <AnimatedBeam containerRef={containerRef} fromRef={div1Ref} toRef={div4Ref} curvature={-40} startYOffset={-10} />
-              <AnimatedBeam containerRef={containerRef} fromRef={div2Ref} toRef={div4Ref} />
-              <AnimatedBeam containerRef={containerRef} fromRef={div3Ref} toRef={div4Ref} curvature={40} startYOffset={10} />
+              {/* Beams from Inputs to Center with staggered delay */}
+              <AnimatedBeam containerRef={containerRef} fromRef={div1Ref} toRef={div4Ref} curvature={-40} startYOffset={-10} delay={0} />
+              <AnimatedBeam containerRef={containerRef} fromRef={div2Ref} toRef={div4Ref} delay={0.15} />
+              <AnimatedBeam containerRef={containerRef} fromRef={div3Ref} toRef={div4Ref} curvature={40} startYOffset={10} delay={0.3} />
 
-              {/* Beams from Center to Outputs */}
-              <AnimatedBeam containerRef={containerRef} fromRef={div4Ref} toRef={div5Ref} curvature={40} endYOffset={10} reverse />
-              <AnimatedBeam containerRef={containerRef} fromRef={div4Ref} toRef={div6Ref} reverse />
-              <AnimatedBeam containerRef={containerRef} fromRef={div4Ref} toRef={div7Ref} curvature={-40} endYOffset={-10} reverse />
+              {/* Beams from Center to Outputs with staggered delay */}
+              <AnimatedBeam containerRef={containerRef} fromRef={div4Ref} toRef={div5Ref} curvature={40} endYOffset={10} reverse delay={0.45} />
+              <AnimatedBeam containerRef={containerRef} fromRef={div4Ref} toRef={div6Ref} reverse delay={0.6} />
+              <AnimatedBeam containerRef={containerRef} fromRef={div4Ref} toRef={div7Ref} curvature={-40} endYOffset={-10} reverse delay={0.75} />
             </>
           )}
         </div>
